8f74064ad3bdea1c070787d1a638bcfcc55e1e36
Janielson Anjos <72242469+Janielson-Anjos@users.noreply.github.com>
2024-11-21 04:17:11 +0000

    fix rota

--- api/json-server.js
+++ api/json-server.js
@@ -1,9 +1,8 @@
 const jsonServer = require('json-server');
-const path = require('path');
 
 // Criar o servidor JSON
-// const router = jsonServer.router('./api/db.json');
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router('./api/db.json');
+
 
 const middlewares = jsonServer.defaults();
 
